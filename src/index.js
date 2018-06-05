75348b5fbbfdbf5f398ca7b99e369de6ab76b842
Martin <w.martin38@icloud.com>
2018-06-05 22:07:14 +0200

    Import Semantic UI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/layout/App';
-import './index.css'
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 const rootEl = document.getElementById('root');
