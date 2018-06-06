f237d6369076d58f8a7eda2e1e68b091073084c4
Martin <w.martin38@icloud.com>
2018-06-06 22:47:20 +0200

    RUD on event

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,20 +1,42 @@
 import React, { Component } from 'react'
 import {Form, Segment, Button} from "semantic-ui-react";
 
+const emptyEvent = {
+    title: '',
+    date: '',
+    city: '',
+    venue: '',
+    hostedBy: ''
+};
+
 class EventForm extends Component{
     state = {
-        event : {
-            title: '',
-            date: '',
-            city: '',
-            venue: '',
-            hostedBy: ''
-        }
+        event: emptyEvent
     };
 
-    onfFormSubmit = (evt) => {
+    componentDidMount() {
+        if(this.props.selectedEvent !== null){
+            this.setState({
+                event: this.props.selectedEvent
+            })
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.selectedEvent !== this.props.selectedEvent){
+            this.setState({
+                event: nextProps.selectedEvent || emptyEvent
+            })
+        }
+    }
+
+    onFormSubmit = (evt) => {
         evt.preventDefault();
-        this.props.createEvent(this.state.event);
+        if(this.state.event.id) {
+            this.props.updateEvent(this.state.event);
+        } else {
+            this.props.createEvent(this.state.event);
+        }
     };
 
     onInputChange = (evt) => {
@@ -30,7 +52,7 @@ class EventForm extends Component{
         const {event} = this.state;
         return (
             <Segment>
-                <Form onSubmit={this.onfFormSubmit}>
+                <Form onSubmit={this.onFormSubmit}>
                     <Form.Field>
                         <label>Event Title</label>
                         <input
