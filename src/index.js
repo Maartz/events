94406ca27754c013c1dd7dd131fc07e466cc2cc2
Martin <w.martin38@icloud.com>
2018-06-10 20:15:10 +0200

    Clean up EventList | Item and Dashboard from functions Add ScrollToTop func from React Router Connect actions for creating, updating and deleting events from the redux store

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import {configureStore} from "./app/store/configureStore";
+import ScrollToTop from './app/common/util/ScrollToTop';
 
 const store = configureStore();
 
@@ -17,11 +18,13 @@ let render = () => {
     ReactDOM.render(
         <Provider store={store}>
             <BrowserRouter>
-                <App />
+                <ScrollToTop>
+                    <App />
+                </ScrollToTop>
             </BrowserRouter>
         </Provider>
         , rootEl);
-}
+};
 
 if(module.hot){
     module.hot.accept('./app/layout/App', () => {
