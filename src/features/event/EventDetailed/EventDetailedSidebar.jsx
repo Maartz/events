c8caa2589beac65adb15ab806ce6b286b019fd04
Martin <w.martin38@icloud.com>
2018-06-10 17:32:55 +0200

    Scaffold pages of EventDetailed | typo in App.jsx causes routing issue fixed

---
+++ src/features/event/EventDetailed/EventDetailedSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EventDetailedSidebar = () => {
+    return (
+        <div>
+            <h1>Sidebard</h1>
+        </div>
+    )
+};
+
+export default EventDetailedSidebar
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import {Segment, Item, Button, Icon, List} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import EventListAttendee from './EventListAttendee';
 
 class EventListItem extends Component {
     render() {
-        const {event, onEventOpen, deleteEvent} = this.props;
+        const {event, deleteEvent} = this.props;
         return (
             <div>
                      <Segment.Group>
@@ -40,8 +41,8 @@ class EventListItem extends Component {
                         <Segment clearing>
                         <span>{event.description}</span>
                           <Button
-                              onClick={onEventOpen(event)}
-                              as="a"
+                              as={Link}
+                              to={`/event/${event.id}`}
                               color="teal"
                               floated="right"
                               content="View"
