5925f51b1d2c2fc615763b2d5142b00dd2ad1220
Martin <w.martin38@icloud.com>
2018-06-14 20:57:21 +0200

    Init of redux thunk for returning function instead of object, dispatching async

--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -1,19 +1,30 @@
-import {DECREMENT_COUNTER, INCREMENT_COUNTER} from "./testConstants";
-import {createReducer } from '../../app/common/util/reducerUtil';
+import {DECREMENT_COUNTER, INCREMENT_COUNTER, COUNTER_ACTION_FINISHED, COUNTER_ACTION_STARTED} from "./testConstants";
+import {createReducer} from '../../app/common/util/reducerUtil';
 
 const initialState = {
-    data: 42
+    data: 42,
+    loading: false
 };
 
 export const incrementCounter = (state, payload) => {
-    return{ ...state, data: state.data + 1};
+    return {...state, data: state.data + 1};
 };
 
 export const decrementCounter = (state, payload) => {
-    return{ ...state, data: state.data - 1};
+    return {...state, data: state.data - 1};
 };
+
+export const counterActionStarted = (state, payload) => {
+    return {...state, loading: true}
+}
+
+export const counterActionFinished = (state, payload) => {
+    return {...state, loading: false}
+}
 
 export default createReducer(initialState, {
     [INCREMENT_COUNTER]: incrementCounter,
-    [DECREMENT_COUNTER]: decrementCounter
+    [DECREMENT_COUNTER]: decrementCounter,
+    [COUNTER_ACTION_STARTED]: counterActionStarted,
+    [COUNTER_ACTION_FINISHED]: counterActionFinished
 });
