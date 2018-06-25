87cd105487f596c2feb099bf9a6550e22dff7aab
Martin <w.martin38@icloud.com>
2018-06-25 23:19:46 +0200

    Import cropper Add states cropImage method

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import {Image, Icon, Segment, Header, Divider, Grid, Button, Card} from 'semantic-ui-react';
 import Dropzone from 'react-dropzone';
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
 
 class PhotosPage extends Component {
     state = {
-        files : [],
-        fileName: ''
+        files: [],
+        fileName: '',
+        cropResult: null,
+        image: {}
     }
 
     onDrop = (files) => {
@@ -13,6 +17,20 @@ class PhotosPage extends Component {
             files,
             fileName: files[0].name
         })
+    };
+
+    cropImage = () => {
+        if (typeof this.refs.cropper.getCroppedCanvas() === 'undefined') {
+            return;
+        }
+
+        this.refs.cropper.getCroppedCanvas().toBlob(blob => {
+            let imageUrl = URL.createObjectURL(blob);
+            this.setState({
+                cropResult: imageUrl,
+                image: blob
+            })
+        }, 'image/jpeg');
     };
 
     render() {
@@ -34,12 +52,26 @@ class PhotosPage extends Component {
                     <Grid.Column width={1}/>
                     <Grid.Column width={4}>
                         <Header sub style={{color: '#53f'}} content='Étape 2 - Redimensionner'/>
+                        {this.state.files[0] &&
+                        <Cropper
+                            style={{height: 200, width: '100%'}}
+                            ref='cropper'
+                            src={this.state.files[0].preview}
+                            aspectRatio={1}
+                            viewMode={0}
+                            dragMode='move'
+                            guides={false}
+                            scalable={true}
+                            cropBoxMovable={true}
+                            cropBoxResizable={true}
+                            crop={this.cropImage}
+                        />}
                     </Grid.Column>
                     <Grid.Column width={1}/>
                     <Grid.Column width={4}>
                         <Header sub style={{color: '#53f'}} content='Étape 3 - Aperçu et upload'/>
                         {this.state.files[0] &&
-                        <Image style={{ minHeight: '200px' , minWidth: '200px'}} src={this.state.files[0].preview}/>
+                        <Image style={{minHeight: '200px', minWidth: '200px'}} src={this.state.cropResult}/>
                         }
                     </Grid.Column>
 
